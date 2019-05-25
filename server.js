cd384780cdc724a0b79b377ce5f047eab965c624
Samuel <js.rosemond@gmail.com>
2019-05-25 17:55:47 -0400

    api routes : GET setup

--- server.js
+++ server.js
@@ -11,5 +11,11 @@ connectDB();
 app.get('/', (req, res) => res.send('API Running'));
 const PORT = process.env.PORT || 5000;
 
-// List on port
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/post', require('./routes/api/posts'));
+
+// Listen on port
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
