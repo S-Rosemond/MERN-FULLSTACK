142070cdad165bebc22d8afa9aceb89bf91f1e6d
Samuel <js.rosemond@gmail.com>
2019-05-25 17:36:01 -0400

    MongoDB connected

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
+// Initial express
 const app = express();
+
+// Connect Database
+connectDB();
+
+// Get request
 app.get('/', (req, res) => res.send('API Running'));
 const PORT = process.env.PORT || 5000;
 
+// List on port
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
