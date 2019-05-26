24021e8b984160d56e9ff7a7efb67859c913a757
Samuel <js.rosemond@gmail.com>
2019-05-25 20:13:25 -0400

    Middleware & validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,37 @@
 const express = require('express');
 const router = express.Router();
 
-// @route   GET api/users
-//@ desc    Test route
+// Validator
+const { check, validationResult } = require('express-validator/check');
+
+// @route   POST api/users
+//@ desc    Register user
 //@access   Public
-router.get('/', (req, res) => res.send('User Route'));
+
+/* 
+Over view: add new param: Array~[] w/ check & chained checker functions. 
+Pass in req in ValidationResult().
+If it is empty ok: 200 
+else it will return errors.
+send a 400 status error chain as json by passing an object with error result error.array() 
+*/
+router.post(
+	'/',
+	[
+		check('name', 'Name is required')
+			.not()
+			.isEmpty(),
+		check('email', 'Please include a valid email address').isEmail(),
+		check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+	],
+	(req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		res.send('User Route');
+	}
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 // Connect Database
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 // Get request
 app.get('/', (req, res) => res.send('API Running'));
 const PORT = process.env.PORT || 5000;
