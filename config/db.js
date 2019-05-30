0fab7084f42b04a434fa1a87847c32838a7cf0c1
Samuel <js.rosemond@gmail.com>
2019-05-29 21:33:38 -0400

    Get all profiles, get profile by user

--- config/db.js
+++ config/db.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-mongoose.set('useFindAndModify', false);
 const config = require('config');
 const db = config.get('mongoURI');
 
@@ -7,7 +6,8 @@ const connectDB = async () => {
 	try {
 		await mongoose.connect(db, {
 			useNewUrlParser: true,
-			useCreateIndex: true
+			useCreateIndex: true,
+			useFindAndModify: false
 		});
 		console.log('MongoDB Connected...');
 	} catch (err) {
