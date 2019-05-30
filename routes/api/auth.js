162c2240a28a8ddbf2488f84ba30c138ddc42520
Samuel <js.rosemond@gmail.com>
2019-05-29 21:08:41 -0400

    Create Profile, + minor fixes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -15,7 +15,7 @@ router.get('/', auth, async (req, res) => {
 		const user = await User.findById(req.user.id).select('-password');
 		res.json(user);
 	} catch (error) {
-		console.log(error);
+		console.error(error);
 		res.status(500).send('Server Error');
 	}
 });
