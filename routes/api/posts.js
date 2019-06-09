92c2f4aa17754425c30d16fa7cbfe503aa7bb22b
Samuel <js.rosemond@gmail.com>
2019-06-08 21:45:51 -0400

    Get all post & Get post by ID

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -45,4 +45,47 @@ router.post(
 	}
 );
 
+// @route   GET api/posts
+//@ desc    GET all Post
+//@access   Private
+router.get('/', auth, async (req, res) => {
+	try {
+		/* If you want to limit users to seeing only their own post you can do :
+		
+		const post = await Post.find({user: req.user.id})
+		
+		outside of a get.('/:id'), depends on functionality wanted. Other get req would then be redundant; depends on functionality desired.
+        */
+		const post = await Post.find().sort({ date: -1 });
+		res.json(post);
+	} catch (error) {
+		console.error(error.message);
+
+		res.satus(500).send('Internal Server Error');
+	}
+});
+
+// @route   GET api/posts/:id
+//@ desc    GET Post by id
+//@access   Private
+router.get('/:id', auth, async (req, res) => {
+	try {
+		// Keeping sort for latest post first
+		const post = await Post.findById(req.params.id).sort({ date: -1 });
+
+		if (!post) {
+			res.status(404).json({ msg: 'Error 404: Post not found' });
+		}
+
+		res.json(post);
+	} catch (error) {
+		console.error(error.message);
+		if (error.kind === 'ObjectId') {
+			res.status(404).json({ msg: 'Error 404: Post not found' });
+		}
+
+		res.satus(500).send('Internal Server Error');
+	}
+});
+
 module.exports = router;
