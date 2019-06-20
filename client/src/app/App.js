4a0018f5a4bd72ef8576d2a5c9bcaf3f214b89e1
Samuel <js.rosemond@gmail.com>
2019-06-20 06:37:28 -0400

    Added Font, fixed a-tag to Link, minor css

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './../components/Layout/NavBar/Navbar';
 import Landing from '../components/Layout/Landing/Landing';
-
+import Login from './../components/auth/Login';
+import Register from './../components/auth/Register';
 
 const App = () => (
-	<React.Fragment>
-	<Navbar/>
-	<Landing/>
-	</React.Fragment>
+	<Router>
+		<React.Fragment>
+			<Navbar />
+			<Route exact path="/" component={Landing} />
+			<Switch>
+				<section className="container">
+					<Route exact path="/login" component={Login} />
+					<Route exact path="/register" component={Register} />
+				</section>
+			</Switch>
+		</React.Fragment>
+	</Router>
 );
 
 export default App;
