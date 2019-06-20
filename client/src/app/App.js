31e325833fdfcfdb1d5a5fb85f242b8c3a78608c
Samuel <js.rosemond@gmail.com>
2019-06-20 06:10:49 -0400

    NavBar, Landing Page React

---
+++ client/src/app/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './../components/Layout/NavBar/Navbar';
+import Landing from '../components/Layout/Landing/Landing';
+
+
+const App = () => (
+	<React.Fragment>
+	<Navbar/>
+	<Landing/>
+	</React.Fragment>
+);
+
+export default App;
