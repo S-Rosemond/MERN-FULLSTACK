5d80b613b0b2a96dc07b9a9196948890953675eb
Samuel <js.rosemond@gmail.com>
2019-06-20 22:09:11 -0400

    Login, minor fixes : add Link to

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-	return <div>Login User</div>;
+	const [formData, setFormData] = useState({
+		email: '',
+		password: ''
+	});
+
+	const { email, password } = formData;
+
+	const changed = event => setFormData({ ...formData, [event.target.name]: event.target.value });
+
+	const submitForm = event => {
+		event.preventDefault();
+		console.log('SUCCESS');
+	};
+
+	return (
+		<React.Fragment>
+			<h1 className="large text-primary">My Account </h1>
+			<p className="lead">
+				<i className="fas fa-user" /> Login To Your Account
+			</p>
+			<form className="form" onSubmit={async event => submitForm(event)}>
+				<div className="form-group">
+					<input
+						required
+						type="email"
+						placeholder="Email Address"
+						name="email"
+						value={email}
+						onChange={event => changed(event)}
+					/>
+				</div>
+				<div className="form-group">
+					<input
+						type="password"
+						placeholder="Password"
+						name="password"
+						minLength="6"
+						value={password}
+						onChange={event => changed(event)}
+					/>
+				</div>
+
+				<input type="submit" className="btn btn-primary" value="login" />
+			</form>
+			<p className="my-1">
+				Don't have an account? <Link to="/register">Register now</Link>
+			</p>
+		</React.Fragment>
+	);
 };
 
 export default Login;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
 	const [formData, setFormData] = useState({
@@ -23,7 +24,7 @@ const Register = () => {
 		if (password !== password2) {
 			console.log('Passwords do not match');
 		} else {
-			console.log(formData);
+			console.log('SUCCESS');
 		}
 	};
 
@@ -33,7 +34,7 @@ const Register = () => {
 			<p className="lead">
 				<i className="fas fa-user" /> Create Your Account
 			</p>
-			<form className="form" onSubmit={event => submitForm(event)}>
+			<form className="form" onSubmit={async event => submitForm(event)}>
 				<div className="form-group">
 					<input
 						type="text"
@@ -80,7 +81,7 @@ const Register = () => {
 				<input type="submit" className="btn btn-primary" value="Register" />
 			</form>
 			<p className="my-1">
-				Already have an account? <a href="login.html">Sign In</a>
+				Already have an account? <Link to="/login">Sign In</Link>
 			</p>
 		</React.Fragment>
 	);
