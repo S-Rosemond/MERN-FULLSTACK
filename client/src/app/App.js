9c45a69ee5b2de72cec8ac4f56fa6a6b03c009c9
Samuel <js.rosemond@gmail.com>
2019-07-06 13:30:41 -0400

    AddExperience and Education front to DB working

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -9,6 +9,8 @@ import Dashboard from './../components/Layout/Dashboard/Dashboard';
 import PrivateRoute from '../components/routing/PrivateRoute';
 import CreateProfile from './../components/Layout/Profile-Form/CreateProfile';
 import EditProfile from './../components/Layout/Profile-Form/EditProfile';
+import AddExperience from './../components/Layout/Profile-Form/AddExperience';
+import AddEducation from './../components/Layout/Profile-Form/AddEduction';
 // Redux
 import { Provider } from 'react-redux';
 import store from '../Store/store';
@@ -37,6 +39,8 @@ const App = () => {
 							<PrivateRoute exact path="/dashboard" component={Dashboard} />
 							<PrivateRoute exact path="/create-profile" component={CreateProfile} />
 							<PrivateRoute exact path="/edit-profile" component={EditProfile} />
+							<PrivateRoute exact path="/add-experience" component={AddExperience} />
+							<PrivateRoute exact path="/add-education" component={AddEducation} />
 						</Switch>
 					</section>
 				</React.Fragment>
