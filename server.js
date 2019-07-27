c43cd2e0973ee76ae2865e77ea32ba50306719c6
Samuel <js.rosemond@gmail.com>
2019-07-27 11:18:52 -0400

    deploy setup, then review

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 // Initialize express
 const app = express();
@@ -10,8 +11,8 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-// Get request
-app.get('/', (req, res) => res.send('API Running'));
+// Get request : remove app.get for production
+
 const PORT = process.env.PORT || 5000;
 
 // Define Routes
@@ -20,5 +21,14 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+// PRODUCTION: serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 // Listen on port
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
